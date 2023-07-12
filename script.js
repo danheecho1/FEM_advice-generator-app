fb26e1f54f1bc512ab0990773fdd3900c9cbaedd
Danny Cho <danheecho@gmail.com>
2023-07-11 17:36:19 -0700

    Added no cache for fetch function. Also removed onclick event from HTML and added an eventlistener in JS to separate HTML and JS better.

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 const number = document.querySelector(".viewport__card__header__id");
 const quote = document.querySelector(".viewport__card__advice");
+const button = document.querySelector(".viewport__card__button");
 
 function getAdvice() {
-	fetch("https://api.adviceslip.com/advice")
+	fetch("https://api.adviceslip.com/advice", {cache: "no-cache"})
 		.then((response) => response.json())
 		.then((data) => {
 			number.innerHTML = "  #  " + data["slip"]["id"];
 			quote.innerHTML = '"' + data["slip"]["advice"] + '"';
 		});
 }
+
+button.addEventListener("click", () => {
+	getAdvice();
+})
 
 /* 
 THIS IS CHAT GPT'S VERSION OF REFACTORED CODE
